6395e74c714f04d2a9150110c0b481ea5f2ab8f0
Tiago Tiede <geschopf@gmail.com>
2019-03-29 13:48:05 -0300

    improving EntriesModule

---
+++ finances/src/app/pages/entries/shared/service/entry.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+import { Entry } from "../model/entry.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+  
+  private apiPath: string = "api/entries";
+
+  constructor (private http: HttpClient) { }
+
+  getAll(): Observable<Array<Entry>>{
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntries)
+    );
+  }
+
+  getById(id: number): Observable<Entry>{
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  create(entry: Entry): Observable<Entry>{
+    return this.http.post(this.apiPath, entry).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  update(entry: Entry): Observable<Entry>{
+    const url = `${this.apiPath}/${entry.id}`;
+
+    return this.http.put(url, entry).pipe(
+      catchError(this.handleError),
+      map(() => entry)
+    );
+  }
+
+  delete(id: number): Observable<any>{
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  private handleError(handleError: any): Observable<any> {
+    console.log("Erro na requisiçáo =>", handleError);
+    return throwError(handleError);
+  }
+
+  private jsonDataToEntries(jsondata: any[]): Array<Entry> {
+    const entries: Array<Entry> = new Array<Entry> ();
+    jsondata.forEach(element => {
+      entries.push(element as Entry);
+    });
+    
+    return entries;
+  }
+
+  private jsonDataToEntry(jsondata: any): Entry {
+    return jsondata as Entry;
+  }  
+}
