82ab3081b8678f4bf245e1ecef732f0976bda17f
Tiago Tiede <geschopf@gmail.com>
2019-03-31 23:12:37 -0300

    adding BaseResourceFormComponent

---
+++ finances/src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -0,0 +1,141 @@
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { BaseResourceModel } from "../../models/base-resource.model"
+import { BaseResourceService } from "../../services/base-resource.service"
+
+import { switchMap } from "rxjs/operators";
+
+import toastr from "toastr";
+
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  isNewForm: boolean = true;
+
+  protected route: ActivatedRoute;
+  protected router: Router;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector: Injector,
+    public resource: T,
+    protected resourceService: BaseResourceService<T>,
+    protected jsonDataToResourceFn: (jsonData) => T
+  ) {
+    this.route = this.injector.get(ActivatedRoute);
+    this.router = this.injector.get(Router);
+    this.formBuilder = this.injector.get(FormBuilder);
+  }
+
+  ngOnInit() {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == "new")
+      this.createResource();
+    else // currentAction == "edit"
+      this.updateResource();
+  }
+  ngAfterContentChecked(): void {
+    this.setPageTittle();
+  }
+
+
+  protected createResource() {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.create(resource)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      )
+  }
+
+
+  protected updateResource() {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.update(resource)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      )
+  }
+
+  protected actionsForError(error) {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
+
+    this.submittingForm = false;
+
+    if (error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
+  }
+  protected actionsForSuccess(resource: T) {
+    toastr.success("Solicitação processada com sucesso!");
+
+    const baseComponentPath: string = this.route.snapshot.parent.url[0].path;
+
+    this.router.navigateByUrl(baseComponentPath, { skipLocationChange: true }).then(
+      () => this.router.navigate([baseComponentPath, resource.id, "edit"])
+    )
+  }
+
+  protected loadResource() {
+    if (this.currentAction == "edit") {
+
+      this.route.paramMap.pipe(
+        switchMap(params => this.resourceService.getById(+params.get("id")))
+      )
+        .subscribe(
+          (resource) => {
+            this.resource = resource;
+            this.resourceForm.patchValue(resource) 
+          },
+          (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
+        )
+    }
+  }
+
+  protected setPageTittle() {
+    if (this.isNewForm)
+      this.pageTitle = this.creationPageTitle();
+    if (!this.isNewForm) {
+      this.pageTitle = this.editionPageTitle();
+    }
+  }
+
+  protected creationPageTitle(): string {
+    return "Novo"
+  }
+
+  protected editionPageTitle(): string {
+    return "Edição"
+  }
+
+
+  protected abstract buildResourceForm(): void;
+
+  protected setCurrentAction() {
+    if (this.route.snapshot.url[0].path == "new") {
+      this.currentAction = "new";
+    }
+    else {
+      this.currentAction = "edit";
+      this.isNewForm = false;
+    }
+  }
+}
