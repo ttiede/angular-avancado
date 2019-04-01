566cca0eca3c69e3d1cc7fe744edaf2c9756ff9d
Tiago Tiede <geschopf@gmail.com>
2019-03-31 23:30:55 -0300

    refactoring CategoryFormComponent

--- finances/src/app/pages/categories/category-form/category-form.component.ts
+++ finances/src/app/pages/categories/category-form/category-form.component.ts
@@ -1,127 +1,39 @@
-import { Component, OnInit, AfterContentChecked } from "@angular/core";
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators
-} from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Component, Injector } from '@angular/core';
+import { Validators } from "@angular/forms";
+
+import { BaseResourceFormComponent } from "../../../shared/components/base-resource-form/base-resource-form.component"
+
 import { Category } from "../shared/models/category.model";
 import { CategoryService } from "../shared/services/category.service";
-import { switchMap } from "rxjs/operators";
-import toastr from "toastr";
+
 
 @Component({
-  selector: "app-category-form",
-  templateUrl: "./category-form.component.html",
-  styleUrls: ["./category-form.component.css"]
+  selector: 'app-category-form',
+  templateUrl: './category-form.component.html',
+  styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit, AfterContentChecked {
-  currentAction: string;
-  categoryForm: FormGroup;
-  pageTitle: string;
-  serverErrorMessages: Array<string>;
-  submittingForm: boolean = false;
-  category: Category = new Category();
-  isNewForm: boolean = true;
-  constructor(
-    private categoryService: CategoryService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder
-  ) { }
+export class CategoryFormComponent extends BaseResourceFormComponent<Category> {
 
-  ngOnInit() {
-    this.setCurrentAction();
-    this.buildCategoryForm();
-    this.loadCategory();
+  constructor(protected categoryService: CategoryService, protected injector: Injector) { 
+    super(injector, new Category(), categoryService, Category.fromJson)
   }
 
-  ngAfterContentChecked(): void {
-    this.setPageTittle();
-  }
 
-  submitForm() {
-    this.submittingForm = true;
-    if (this.isNewForm)
-      this.createCategory();
-    else
-      this.updateCategory();
-  }
-
-  private updateCategory(): any {
-    const category: Category = Object.assign(new Category(), this.categoryForm.value);
-    this.categoryService.update(category).subscribe(
-      (category) => this.actionsForSucesses(category),
-      (error) => this.actionsForError(error)
-    );
-  }
-
-  private createCategory() {
-    const category: Category = Object.assign(new Category(), this.categoryForm.value);
-    this.categoryService.create(category).subscribe(
-      (category) => this.actionsForSucesses(category),
-      (error) => this.actionsForError(error)
-    );
-  }
-
-  private actionsForError(error: any): void {
-    toastr.error("Ocorreu um erro ao processar a sua solicitação");
-
-    this.submittingForm = false;
-    if (error.status == 422)
-      this.serverErrorMessages = JSON.parse(error._body).errors;
-    else
-      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."];
-  }
-  private actionsForSucesses(category: Category): void {
-    toastr.success("Solicitação processada com sucesso");
-    this.router.navigateByUrl("categories", { skipLocationChange: true });
-    this.router.navigateByUrl("categories", { skipLocationChange: true }).then(
-      () => this.router.navigate(["categories", category.id, "edit"])
-    );
-  }
-
-  private loadCategory() {
-    if (!this.isNewForm) {
-      this.route.paramMap
-        .pipe(
-          switchMap(params => this.categoryService.getById(+params.get("id")))
-        )
-        .subscribe(
-          category => {
-            this.category = category;
-            this.categoryForm.patchValue(category);
-          },
-          error => alert("Ocorreu um erro no servidor, tente mais tarde.")
-        );
-    }
-  }
-
-  private setPageTittle() {
-    if (this.isNewForm)
-      this.pageTitle = "Cadastro de nova Categoria";
-    if (!this.isNewForm) {
-      const categoryName = this.category.name || "";
-      this.pageTitle = "Editando Categoria: " + categoryName;
-    }
-  }
-
-  private buildCategoryForm(): any {
-    this.categoryForm = this.formBuilder.group({
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null]
     });
   }
 
-  private setCurrentAction() {
-    if (this.route.snapshot.url[0].path == "new") {
-      this.currentAction = "new";
-    }
-    else {
-      this.currentAction = "edit";
-      this.isNewForm = false;
-    }
+
+  protected creationPageTitle(): string {
+    return "Cadastro de Nova Categoria";
+  }
+
+  protected editionPageTitle(): string {
+    const categoryName = this.resource.name || "";
+    return "Editando Categoria: " + categoryName;
   }
 }
