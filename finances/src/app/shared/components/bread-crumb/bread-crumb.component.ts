363693b8cbd9c92d49885cc9f0152c5a84066ef3
Tiago Tiede <geschopf@gmail.com>
2019-04-01 23:04:11 -0300

    improving bread-crumb component

--- finances/src/app/shared/components/bread-crumb/bread-crumb.component.ts
+++ finances/src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
 
- interface BreadCrumbItem {
+interface BreadCrumbItem {
   text: string;
   link?: string;
 }
 
- @Component({
+@Component({
   selector: 'app-bread-crumb',
   templateUrl: './bread-crumb.component.html',
   styleUrls: ['./bread-crumb.component.css']
 })
 export class BreadCrumbComponent implements OnInit {
 
-   @Input() items: Array<BreadCrumbItem> = [];
+  @Input() items: Array<BreadCrumbItem> = [];
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() {
+  ngOnInit() {
   }
 
-   isTheLastItem(item: BreadCrumbItem): boolean {
+  isTheLastItem(item: BreadCrumbItem): boolean {
     const index = this.items.indexOf(item);
     return index + 1 == this.items.length;
   }
 
- }
+}
